05e563c20f46187e560f3acf8c578395db01d42f
Arlene <arlenesgo1998@gmail.com>
2024-05-09 12:52:56 -0700

    Primer commit

---
+++ lugar/lugar.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const getLugarLatLng = async(dir) => {
+    const encodedUrl = encodeURI(dir);
+    //console.log(encodedUrl);    
+    const instance = axios.create({
+        baseURL: `https://open-weather13.p.rapidapi.com/city/${encodedUrl}`,
+        headers: { 'X-RapidAPI-Key': 'cfc78e9071msh532adff0f2fa65cp13b8e8jsn5a7f26ee6b69' }
+    });
+    const resp = await instance.get();
+    if (resp.data.length === 0) {
+        throw Error(`No hay resultados para ${direccion}`);
+    }
+    const data = resp.data;
+    const direccion = data.name;
+    const lat = data.coord.lat;
+    const lng = data.coord.lon
+    return {
+        direccion,
+        lat,
+        lng
+    }
+}
+module.exports = {
+    getLugarLatLng
+}
